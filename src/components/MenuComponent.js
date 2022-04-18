f9fc9317a75ff6fe4f478e7439e01aa771b09f83
hakeem <hakeemali34@gmail.com>
2022-04-18 15:31:22 -0500

    Components Part 2 - updated

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -37,8 +37,8 @@ class Menu extends Component {
 
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                <div className="col-12 col-md-5 m-1">
+                    <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name}/>
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -54,7 +54,9 @@ class Menu extends Component {
                     {menu}
                 </div>
                 <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
+                    <div  className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.state.selectedDish)}
+                    </div>    
                 </div>
             </div>
         );
